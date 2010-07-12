3e7feaedfeb620b62e16579d3d048182c93bf3b6
Sam Warmuth <sam@samwarmuth.com>
2010-07-12 15:12:55 -0500

    a whole lot of progress

--- app/routes/home.rb
+++ app/routes/home.rb
@@ -1,5 +1,30 @@
 class Main
   get "/" do
-    erb :home
+    @big_header = true
+    haml :'public/home', :layout => :public
   end
+  get "/about" do
+    haml :'public/about', :layout => :public
+  end
+  get "/css/style.css" do
+    content_type 'text/css', :charset => 'utf-8'
+    sass :style
+  end
+  get "/css/blue.css" do
+    content_type 'text/css', :charset => 'utf-8'
+    sass :blue
+  end
+  get "/login" do
+    haml :login, :layout => false
+  end
+  get "/index" do
+    haml :dashboard
+  end
+  get "/dashboard" do
+    haml :dashboard, :layout => false
+  end
+  get "/manifest" do
+    haml :manifest, :layout => false
+  end
+    
 end
