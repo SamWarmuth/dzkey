cb61bc23ba59f672a4566eec1bd61804914ba84a
Kent McCullough <posideon.sian@gmail.com>
2010-08-03 14:47:41 -0500

    Random updates

--- app/models/rig.rb
+++ app/models/rig.rb
@@ -2,7 +2,10 @@ class Rig < CouchRest::ExtendedDocument
   use_database COUCHDB_SERVER
   
   property :name
-  property :type
+  property :id
+  property :container_type
+  property :reserve_type
+  property :main_type
   
   property :last_main_repack #or next needed? Are all packs the same?
   property :last_reserve_repack #or next needed.
