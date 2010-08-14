fafdda76b85bc2716f5e7f1f6794d9e2d50c00a3
Sam Warmuth <sam@samwarmuth.com>
2010-08-14 03:49:04 -0500

    significant progress on manifesting...still only 70% though.

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -17,20 +17,31 @@ class Aircraft < CouchRest::ExtendedDocument
   property :nxt_flight  
   property :departure_time
   
+  def reset_flights
+    self.cur_flight = nil
+    self.nxt_flight = nil
+    self.save
+    return true
+  end
+  
   def current_flight
     return self.cur_flight if self.cur_flight
     flights = Flight.all.find_all{|f| f.aircraft_id == self.id}
     return nil if flights.nil?
-    self.cur_flight = flights.sort_by{|f| f.number}.first.id
+    flight = flights.sort_by{|f| f.number}.first
+    return nil unless flight && flight.cleared
+    self.cur_flight = flight.id
+    self.departure_time = (Time.now + self.cycle_minutes.to_i*60).to_s
     self.save
     return self.cur_flight
   end
   def next_flight
     return self.nxt_flight if self.nxt_flight
     self.cur_flight = self.current_flight unless self.cur_flight
+    return nil unless self.cur_flight
     flights = Flight.all.find_all{|f| f.aircraft_id == self.id}
     return nil if flights.nil?
-    self.nxt_flight = flights.find_all{|f| f.object_id != self.cur_flight}.sort_by{|f| f.number}.first.id
+    self.nxt_flight = flights.find_all{|f| f.id != self.cur_flight}.sort_by{|f| f.number}.first.id
     return self.nxt_flight
   end
   
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,9 @@ class Flight < CouchRest::ExtendedDocument
   use_database COUCHDB_SERVER
   
   property :number
+  property :aircraft_prefix
+  
+  property :cleared
   property :taken_off
   property :landed
   
